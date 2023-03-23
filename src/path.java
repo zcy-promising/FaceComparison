bf6672b7626156bd41670b217e983ba79f60dabd
ZCY <1399328545@qq.com>
2023-03-23 20:45:38 +0800

    更新相对路径

--- src/path.java
+++ src/path.java
@@ -1,7 +1,7 @@
 //存放固定地址
 public class path {
     //openCV自带的人脸模型数据
-    public static final String haarcascades="D:\\Code\\Project\\face_comparison\\src\\main\\resources\\static\\haarcascade_frontalface_default.xml";
+    public static final String haarcascades=".\\static\\haarcascade_frontalface_default.xml";
     //人脸存放位置
     public static final String faceSave=".\\static\\saveFace\\";
 
