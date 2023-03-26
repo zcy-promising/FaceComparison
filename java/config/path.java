8023acd9b060deb28b9d3a9202423912287004f1
ZCY <1399328545@qq.com>
2023-03-26 13:28:20 +0800

    各项路径配置

--- java/config/path.java
+++ java/config/path.java
@@ -1,11 +1,16 @@
 package config;
 
-//存放固定地址
+//各项路径配置
 public class path {
     //openCV自带的人脸模型数据
     public static final String haarcascades=".\\static\\haarcascade_frontalface_default.xml";
     //人脸存放位置
     public static final String faceSave=".\\static\\saveFace\\";
-
+    //数据库名
+    public static final String userName="root";
+    //数据库密码
+    public static final String password="123456";
+    //数据库连接地址
+    public static final String url="jdbc:mysql://localhost:3306/face_manage";
 
 }
