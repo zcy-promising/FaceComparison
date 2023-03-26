cf18d5b87664d48049a8a7abc0354d7ad4071ec9
ZCY <1399328545@qq.com>
2023-03-26 11:06:00 +0800

    数据库更新

--- static/SQL/face_manage.sql
+++ static/SQL/face_manage.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 23/03/2023 22:47:14
+ Date: 26/03/2023 10:54:56
 */
 
 SET NAMES utf8mb4;
@@ -25,13 +25,14 @@ CREATE TABLE `manage`  (
   `mid` int NOT NULL,
   `mname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `mpassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `delete` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`mid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of manage
 -- ----------------------------
-INSERT INTO `manage` VALUES (1, '张三', '654321');
+INSERT INTO `manage` VALUES (1, '张三', '654321', 0);
 
 -- ----------------------------
 -- Table structure for user
@@ -41,12 +42,13 @@ CREATE TABLE `user`  (
   `uid` int NOT NULL AUTO_INCREMENT,
   `uname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `upassword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `delete` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 100002 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (100001, '张三', '123456');
+INSERT INTO `user` VALUES (100001, '张三', '123456', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
